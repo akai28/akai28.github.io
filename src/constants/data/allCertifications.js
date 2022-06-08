137938a42b653233f745ad52875fa9f0856f060f
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2022-06-08 16:12:10 +0100

    added certifications data

---
+++ src/constants/data/allCertifications.js
@@ -0,0 +1,66 @@
+export const allCertifications = [
+  {
+    id: 1,
+    certificationName: "Google UX Design Professional Certificate",
+    certificateProvider: "Coursera",
+    courses: [
+      {
+        cid: 3,
+        courseName: "Build Wireframes and Low-Fidelity Prototypes",
+        achievement: "Achieved 94.44%",
+        projects: [
+          {
+            pid: 1,
+            projectName: "Create storyboards",
+            projectLink:
+              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+          },
+          {
+            pid: 2,
+            projectName: "Create digital wireframes",
+            projectLink:
+              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+          },
+          {
+            pid: 3,
+            projectName: "Build a low-fidelity prototype",
+            projectLink:
+              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+          },
+        ],
+      },
+      {
+        cid: 2,
+        courseName:
+          "Start the UX Design Process: Empathize, Define, and Ideate",
+        achievement: "Achieved 94.33%",
+        projects: [
+          {
+            pid: 1,
+            projectName: "Create personas",
+            projectLink:
+              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+          },
+          {
+            pid: 2,
+            projectName: "Create a user journey map",
+            projectLink:
+              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+          },
+          {
+            pid: 3,
+            projectName: "Create a competitive audit",
+            projectLink:
+              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+          },
+        ],
+      },
+      {
+        cid: 1,
+        courseName: "Foundations of User Experience (UX) Design",
+        achievement: "Achieved 94.84%",
+        projects: [],
+      },
+    ],
+  },
+];
--- src/pages/Home/Courses.jsx
+++ src/pages/Home/Courses.jsx
@@ -1,6 +1,7 @@
 // @ts-check
 
 import { FiExternalLink } from "react-icons/fi";
+import { allCertifications } from "../../constants/data/allCertifications";
 
 const Courses = () => {
   return (
@@ -8,58 +9,46 @@ const Courses = () => {
       <h2 className="text-4xl font-medium text-slate-800">Courses </h2>
       <main className="grid grid-cols-1">
         <div className="text-cyan-900">
-          <p className="text-xl font-bold mt-4">Google UX Design Professional Certificate</p>
-          <p className="text-lg font-light italic">Coursera</p>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2">
-          <div className="bg-cyan-100 border-blue-300 border-2 rounded-lg">
-            <div className="p-2">
-              <p className="font-semibold">3. Build Wireframes and Low-Fidelity Prototypes</p>
-              <p className="font-light">Achieved 94.44%</p>
-              <div className="grid grid-cols-1 gap-4 mt-2">
-                <div className="bg-cyan-200 border-blue-400 border-2 rounded-lg">
-                  <div className="p-2">
-                    <p className="font-medium">Projects:</p>
-                    <ul className="space-y-2">
-                      <li className="flex"> &#8618; Create storyboards<span className="pl-2"><FiExternalLink />
-                      </span></li>
-                      <li className="flex"> &#8618; Create digital wireframes<span className="pl-2"><FiExternalLink />
-                      </span></li>
-                      <li className="flex"> &#8618; Build a low-fidelity prototype<span className="pl-2"><FiExternalLink />
-                      </span></li>
-                    </ul>
+          {allCertifications.map(({ id, certificationName, certificateProvider, courses }) => (
+            <div key={id}>
+              <p className="text-xl font-bold mt-4">{certificationName}</p>
+              <p className="text-lg font-light italic">{certificateProvider}</p>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2">
+                {courses.map(({ cid, courseName, achievement, projects }) => (
+                  <div key={cid}>
+                    <div className="bg-cyan-100 border-blue-300 border-2 rounded-lg">
+                      <div className="p-2">
+                        <p className="font-semibold">{cid}. {courseName}</p>
+                        <p className="font-light">{achievement}</p>
+
+                        {projects.length > 0 ? (
+                          <div className="grid grid-cols-1 gap-4 mt-2">
+                            <div className="bg-cyan-200 border-blue-400 border-2 rounded-lg">
+                              <div className="p-2">
+                                <p className="font-medium">Projects:</p>
+                                <ul className="space-y-2">
+                                  {projects.map(({ pid, projectName, projectLink }) => (
+                                    <li key={pid} className="flex"> &#8618;
+                                      <a href={projectLink} className="flex">{projectName}
+                                        <span className="pl-2"><FiExternalLink /></span>
+                                      </a>
+                                    </li>
+                                  ))}
+                                </ul>
+                              </div>
+                            </div>
+                          </div>
+                        ) : null}
+
+                      </div>
+                    </div>
                   </div>
-                </div>
+                ))}
               </div>
+
             </div>
-          </div>
-          <div className="bg-cyan-100 border-blue-300 border-2 rounded-lg">
-            <div className="p-2">
-              <p className="font-semibold">2. Start the UX Design Process: Empathize, Define, and Ideate</p>
-              <p className="font-light">Achieved 94.33%</p>
-              <div className="grid grid-cols-1 gap-4 mt-2">
-                <div className="bg-cyan-200 border-blue-400 border-2 rounded-lg">
-                  <div className="p-2">
-                    <p className="font-medium">Projects:</p>
-                    <ul className="space-y-2">
-                      <li className="flex"> &#8618; Create personas <span className="pl-2"><FiExternalLink />
-                      </span></li>
-                      <li className="flex"> &#8618; Create a user journey map<span className="pl-2"><FiExternalLink />
-                      </span></li>
-                      <li className="flex"> &#8618; Create a competitive audit<span className="pl-2"><FiExternalLink />
-                      </span></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="bg-cyan-100 border-blue-300 border-2 rounded-lg">
-            <div className="p-2">
-              <p className="font-semibold">1. Foundations of User Experience (UX) Design</p>
-              <p className="font-light">Achieved 94.84%</p>
-            </div>
-          </div>
+          ))}
         </div>
       </main>
     </section>
