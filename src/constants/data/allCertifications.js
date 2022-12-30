d14932e113b74215d84c4b16e32d86aa744fea65
akai28 <45823160+akai28@users.noreply.github.com>
2022-12-30 18:32:23 +0530

    added certification 4, 5

--- src/constants/data/allCertifications.js
+++ src/constants/data/allCertifications.js
@@ -5,6 +5,50 @@ export const allCertifications = [
     certificateProvider: "Coursera",
     courses: [
       {
+        cid: 5,
+        courseName: "Create High-Fidelity Designs and Prototypes in Figma",
+        achievement: "Achieved 97.96%",
+        projects: [
+          {
+            pid: 1,
+            projectName: "WCreate a high-fidelity prototype",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
+          },
+          {
+            pid: 2,
+            projectName: "Create a case study for your portfolio",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
+          }
+        ]
+      },
+      {
+        cid: 4,
+        courseName: "Conduct UX Research and Test Early Concepts",
+        achievement: "Achieved 96.42%",
+        projects: [
+          {
+            pid: 1,
+            projectName: "Build a research plan",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
+          },
+          {
+            pid: 2,
+            projectName: "Conduct a usability study",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
+          },
+          {
+            pid: 3,
+            projectName: "Turn observations into insights",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
+          },
+          {
+            pid: 4,
+            projectName: "Create project research presentation",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
+          }
+        ]
+      },
+      {
         cid: 3,
         courseName: "Build Wireframes and Low-Fidelity Prototypes",
         achievement: "Achieved 94.44%",
@@ -12,55 +56,48 @@ export const allCertifications = [
           {
             pid: 1,
             projectName: "Create storyboards",
-            projectLink:
-              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
           },
           {
             pid: 2,
             projectName: "Create digital wireframes",
-            projectLink:
-              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
           },
           {
             pid: 3,
             projectName: "Build a low-fidelity prototype",
-            projectLink:
-              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
-          },
-        ],
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
+          }
+        ]
       },
       {
         cid: 2,
-        courseName:
-          "Start the UX Design Process: Empathize, Define, and Ideate",
+        courseName: "Start the UX Design Process: Empathize, Define, and Ideate",
         achievement: "Achieved 94.33%",
         projects: [
           {
             pid: 1,
             projectName: "Create personas",
-            projectLink:
-              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
           },
           {
             pid: 2,
             projectName: "Create a user journey map",
-            projectLink:
-              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
           },
           {
             pid: 3,
             projectName: "Create a competitive audit",
-            projectLink:
-              "https://www.coursera.org/learn/ux-design-professional/home/welcome",
-          },
-        ],
+            projectLink: "https://www.coursera.org/learn/ux-design-professional/home/welcome"
+          }
+        ]
       },
       {
         cid: 1,
         courseName: "Foundations of User Experience (UX) Design",
         achievement: "Achieved 94.84%",
-        projects: [],
-      },
-    ],
-  },
-];
+        projects: []
+      }
+    ]
+  }
+]
