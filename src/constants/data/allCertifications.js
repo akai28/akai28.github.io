8fc4a04a4ad4fc55c8de32d32190d016d091cecb
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2023-01-01 10:27:41 +0000

    Bug Fix - exported allCertifications

--- src/constants/data/allCertifications.js
+++ src/constants/data/allCertifications.js
@@ -1,4 +1,4 @@
-[
+export const allCertifications = [
   {
     id: 1,
     certificationName: "Google UX Design Professional Certificate",
@@ -6,14 +6,16 @@
     courses: [
       {
         cid: 7,
-        courseName: "Design a User Experience for Social Good & Prepare for Jobs",
+        courseName:
+          "Design a User Experience for Social Good & Prepare for Jobs",
         projects: [
           {
             pid: 1,
             projectName: "Case Study",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_Portfolio_Project_Case_Study.pdf"
-          }
-        ]
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_Portfolio_Project_Case_Study.pdf",
+          },
+        ],
       },
       {
         cid: 6,
@@ -23,19 +25,22 @@
           {
             pid: 1,
             projectName: "Create a wireframes for website",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_Create_Website_wireframe.pdf"
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_Create_Website_wireframe.pdf",
           },
           {
             pid: 2,
             projectName: "Create low-fidelity prototype for website",
-            projectLink: "https://www.figma.com/proto/sKuCOnjUpZA2qinqlKyw8O/Restaurant?node-id=1%3A3&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A3"
+            projectLink:
+              "https://www.figma.com/proto/sKuCOnjUpZA2qinqlKyw8O/Restaurant?node-id=1%3A3&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A3",
           },
           {
             pid: 3,
             projectName: "Create high-fidelity prototype for website",
-            projectLink: "https://www.figma.com/proto/sKuCOnjUpZA2qinqlKyw8O/Restaurant?node-id=35%3A3&scaling=scale-down&page-id=35%3A2&starting-point-node-id=35%3A3"
-          }
-        ]
+            projectLink:
+              "https://www.figma.com/proto/sKuCOnjUpZA2qinqlKyw8O/Restaurant?node-id=35%3A3&scaling=scale-down&page-id=35%3A2&starting-point-node-id=35%3A3",
+          },
+        ],
       },
       {
         cid: 5,
@@ -45,14 +50,16 @@
           {
             pid: 1,
             projectName: "Create a high-fidelity prototype",
-            projectLink: "https://www.figma.com/proto/bsROXUzsDj7G4ysQvTKC7A/Bank-App-Wireframe?node-id=348%3A16&scaling=scale-down&page-id=301%3A2&starting-point-node-id=301%3A3"
+            projectLink:
+              "https://www.figma.com/proto/bsROXUzsDj7G4ysQvTKC7A/Bank-App-Wireframe?node-id=348%3A16&scaling=scale-down&page-id=301%3A2&starting-point-node-id=301%3A3",
           },
           {
             pid: 2,
             projectName: "Create a case study for your portfolio",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_Portfolio_Project_Case_Study.pdf"
-          }
-        ]
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_Portfolio_Project_Case_Study.pdf",
+          },
+        ],
       },
       {
         cid: 4,
@@ -62,24 +69,28 @@
           {
             pid: 1,
             projectName: "Build a research plan",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_UX_Research_Study_Plan.pdf"
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_UX_Research_Study_Plan.pdf",
           },
           {
             pid: 2,
             projectName: "Conduct a usability study",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_Usability_Study.pdf"
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_Usability_Study.pdf",
           },
           {
             pid: 3,
             projectName: "Turn observations into insights",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_Insight_Identification.pdf"
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_Insight_Identification.pdf",
           },
           {
             pid: 4,
             projectName: "Create project research presentation",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_Create_Research_Presentation.pdf"
-          }
-        ]
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_Create_Research_Presentation.pdf",
+          },
+        ],
       },
       {
         cid: 3,
@@ -89,48 +100,55 @@
           {
             pid: 1,
             projectName: "Create storyboards",
-            projectLink: "/assets/projects/Google_UX_design_Storyboard_(Big_Picture).pdf"
+            projectLink:
+              "/assets/projects/Google_UX_design_Storyboard_(Big_Picture).pdf",
           },
           {
             pid: 2,
             projectName: "Create digital wireframes",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_Wireframe.pdf"
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_Wireframe.pdf",
           },
           {
             pid: 3,
             projectName: "Build a low-fidelity prototype",
-            projectLink: "https://www.figma.com/proto/bsROXUzsDj7G4ysQvTKC7A/Bank-App-Wireframe?node-id=17%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=17%3A2"
-          }
-        ]
+            projectLink:
+              "https://www.figma.com/proto/bsROXUzsDj7G4ysQvTKC7A/Bank-App-Wireframe?node-id=17%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=17%3A2",
+          },
+        ],
       },
       {
         cid: 2,
-        courseName: "Start the UX Design Process: Empathize, Define, and Ideate",
+        courseName:
+          "Start the UX Design Process: Empathize, Define, and Ideate",
         achievement: "Achieved 94.33%",
         projects: [
           {
             pid: 1,
             projectName: "Create personas",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_Persona.pdf"
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_Persona.pdf",
           },
           {
             pid: 2,
             projectName: "Create a user journey map",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate_User_Journey_Map.pdf"
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate_User_Journey_Map.pdf",
           },
           {
             pid: 3,
             projectName: "Create a competitive audit",
-            projectLink: "/assets/projects/Google_UX_Design_Certificate _Competitive_audit.pdf"
-          }
-        ]
+            projectLink:
+              "/assets/projects/Google_UX_Design_Certificate _Competitive_audit.pdf",
+          },
+        ],
       },
       {
         cid: 1,
         courseName: "Foundations of User Experience (UX) Design",
         achievement: "Achieved 94.84%",
-        projects: []
-      }
-    ]
-  }
-]
+        projects: [],
+      },
+    ],
+  },
+];
