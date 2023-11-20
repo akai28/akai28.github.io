6209162e5a2055a2e3e9945f42e3b1b29038bf08
utk09-NCL <utkarsh.t0298@gmail.com>
2023-11-20 18:58:53 +0000

    image update

--- main.js
+++ main.js
@@ -1,37 +1,50 @@
 // main.js
 document.addEventListener("DOMContentLoaded", function () {
+  // Function to handle successful response
+  function handleResponse(response, targetElement) {
+    if (response.ok) {
+      response
+        .text()
+        .then(
+          (html) => (document.getElementById(targetElement).innerHTML = html)
+        );
+    } else {
+      // If response is not ok, fetch the 404 page
+      fetch("404.html")
+        .then((response) => response.text())
+        .then(
+          (html) => (document.getElementById(targetElement).innerHTML = html)
+        );
+    }
+  }
+
   // Fetch and insert navbar content
   fetch("navbar.html")
-    .then((response) => response.text())
-    .then((html) => (document.getElementById("navbar").innerHTML = html));
+    .then((response) => handleResponse(response, "navbar"))
+    .catch((error) => console.error("Error fetching navbar:", error));
 
   // Fetch and insert skills content
   fetch("skills.html")
-    .then((response) => response.text())
-    .then((html) => (document.getElementById("skills").innerHTML = html));
+    .then((response) => handleResponse(response, "skills"))
+    .catch((error) => console.error("Error fetching skills:", error));
 
   // Fetch and insert description content
   fetch("description.html")
-    .then((response) => response.text())
-    .then((html) => (document.getElementById("description").innerHTML = html));
+    .then((response) => handleResponse(response, "description"))
+    .catch((error) => console.error("Error fetching description:", error));
 
   // Fetch and insert courses content
   fetch("courses.html")
-    .then((response) => response.text())
-    .then((html) => (document.getElementById("courses").innerHTML = html));
+    .then((response) => handleResponse(response, "courses"))
+    .catch((error) => console.error("Error fetching courses:", error));
 
   // Fetch and insert projects content
   fetch("projects.html")
-    .then((response) => response.text())
-    .then((html) => (document.getElementById("projects").innerHTML = html));
-
-  // Fetch and insert contact content
-  // fetch("contact.html")
-  //   .then((response) => response.text())
-  //   .then((html) => (document.getElementById("contact").innerHTML = html));
+    .then((response) => handleResponse(response, "projects"))
+    .catch((error) => console.error("Error fetching projects:", error));
 
   // Fetch and insert footer content
   fetch("footer.html")
-    .then((response) => response.text())
-    .then((html) => (document.getElementById("footer").innerHTML = html));
+    .then((response) => handleResponse(response, "footer"))
+    .catch((error) => console.error("Error fetching footer:", error));
 });
